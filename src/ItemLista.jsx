4f9dd25d009cb52c55cab05ac5905cc04e7d0edf
SamuelRicardos <samuelric4rdo@gmail.com>
2025-04-03 01:08:29 -0300

    alteracao de butoes e instalacao de lucide react

--- src/ItemLista.jsx
+++ src/ItemLista.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Pencil, Trash2 } from "lucide-react";
 
 const ItemLista = ({ itemLista, listaMercado, setListaMercado }) => {
   const removerItemDaLista = () => {
@@ -13,21 +14,21 @@ const ItemLista = ({ itemLista, listaMercado, setListaMercado }) => {
   };
 
   return (
-    <div className="flex items-center p-1 rounded-lg w-full gap-2">
+    <div className="flex items-center p-1 rounded-lg w-full gap-3 bg-gray-100">
       <span className="text-gray-800 font-medium flex-1 text-left" aria-label={`Item da lista: ${itemLista}`}>{itemLista}</span>
       <button
-        className="transition px-4 py-2 text-white bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 shadow-md cursor-pointer"
+        className="transition p-2 text-white bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 shadow-md cursor-pointer"
         onClick={atualizarItemDaLista}
         aria-label={`Atualizar ${itemLista} da lista`}
       >
-        Editar
+        <Pencil size={18} />
       </button>
       <button
-        className="transition px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 shadow-md cursor-pointer"
+        className="transition p-2 text-white bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 shadow-md cursor-pointer"
         onClick={removerItemDaLista}
         aria-label={`Remover ${itemLista} da lista`}
       >
-        Remover
+        <Trash2 size={18} />
       </button>
     </div>
   );
