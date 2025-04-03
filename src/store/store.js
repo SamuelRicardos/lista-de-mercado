0c30904124168b30f9f268375314db6df5d4a227
SamuelRicardos <samuelric4rdo@gmail.com>
2025-04-03 01:40:57 -0300

    adicao de localstorage e alteracoes no zustand

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,26 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useMercadoStore = create((set) => ({
-  listaMercado: [],
-  adicionarItem: (item) =>
-    set((state) => ({ listaMercado: [...state.listaMercado, item] })),
-  removerItem: (item) =>
-    set((state) => ({
-      listaMercado: state.listaMercado.filter((i) => i !== item),
-    })),
-  atualizarItem: (oldItem, newItem) =>
-    set((state) => ({
-      listaMercado: state.listaMercado.map((i) =>
-        i === oldItem ? newItem : i
-      ),
-    })),
-}));
+export const useMercadoStore = create(
+  persist(
+    (set) => ({
+      listaMercado: [],
+      adicionarItem: (item) =>
+        set((state) => ({ listaMercado: [...state.listaMercado, item] })),
+      removerItem: (item) =>
+        set((state) => ({
+          listaMercado: state.listaMercado.filter((i) => i !== item),
+        })),
+      atualizarItem: (itemAntigo, itemNovo) =>
+        set((state) => ({
+          listaMercado: state.listaMercado.map((item) =>
+            item === itemAntigo ? itemNovo : item
+          ),
+        })),
+    }),
+    {
+      name: "mercado-storage",
+      getStorage: () => localStorage,
+    }
+  )
+);
